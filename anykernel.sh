fbee988ccc0a5dbce1eb02dfc812fb5ac72fcf68
Lop <abhishek001konni@gmail.com>
2022-09-04 17:06:27 +0530

    AK3: remove supported versions

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ device.name2=curtana
 device.name3=excalibur
 device.name4=gram
 device.name5=joyeuse
-supported.versions=11.0-12.0
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
