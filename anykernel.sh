fabfa202d327eb863a28f64a699695d4d52e4a22
userariii <abhattacharjee717@gmail.com>
2023-09-13 12:28:52 +0530

    AK3: Welcome RedCherry+
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Forza Kernel by Lopster_70
+kernel.string=RedCherry+ Kernel by CRUECY
 do.devicecheck=1
 do.modules=0
 do.systemless=1
