7420c299250e8cce6342618b4f56071db7409071
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-09-13 12:28:52 +0530

    AK3: Adapt the device & Kernel info for Nokia 660-family
    Co-Authored-by: userariii <abhattacharjee717@gmail.com>
    Co-Authored-by: RainySorcerer <beastdark704@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,20 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Forza Kernel by Lopster_70
-kernel.for=KernelForDriver
-kernel.compiler=AtomSemx
-kernel.made=Kunmun
-kernel.version=4.19.xxx
-message.word=bruh
+kernel.string=RedCherry+ Kernel
+kernel.for=EAS
+kernel.compiler=Aosp Clang 17
+kernel.made=CRUECY & RainySorcerer
+message.word==Thank you for installing RedCherry+ Kernel
 build.date=2069
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=miatoll
-device.name2=curtana
-device.name3=excalibur
-device.name4=gram
-device.name5=joyeuse
+device.name1=PL2
+device.name2=DRG
+device.name3=B2N
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -30,7 +27,6 @@ supported.vendorpatchlevels=
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
 
 ### AnyKernel install
 ## boot files attributes
@@ -58,18 +54,17 @@ dump_boot;
 write_boot;
 ## end boot install
 
-
 ## init_boot files attributes
 #init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+#set_perm_recursive 0 0 755 644 $ramdisk/*;
+#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 #} # end attributes
 
 # init_boot shell variables
-#BLOCK=init_boot;
-#IS_SLOT_DEVICE=auto;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+#block=init_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
 
 # reset for init_boot patching
 #reset_ak;
@@ -82,10 +77,10 @@ write_boot;
 
 
 ## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+#block=vendor_kernel_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
 
 # reset for vendor_kernel_boot patching
 #reset_ak;
@@ -99,15 +94,15 @@ write_boot;
 
 ## vendor_boot files attributes
 #vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+#set_perm_recursive 0 0 755 644 $ramdisk/*;
+#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 #} # end attributes
 
 # vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+#patch_vbmeta_flag=auto;
 
 # reset for vendor_boot patching
 #reset_ak;
@@ -117,4 +112,3 @@ write_boot;
 
 #write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
-
